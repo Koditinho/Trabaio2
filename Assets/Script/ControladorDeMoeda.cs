9d5bf3cf130ee537f5c41434bd083312d2a362a0
aaaa <266051778+Koditinho@users.noreply.github.com>
2026-06-26 16:17:35 -0300

    JojoooEFEITO

--- Assets/Script/ControladorDeMoeda.cs
+++ Assets/Script/ControladorDeMoeda.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class ControladorDeMoeda : MonoBehaviour
 {
+    public GameObject coinParticle;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Jogador"))
         {
             Pontuacao pontos = other.GetComponent<Pontuacao>();
             pontos.AddPoint();
+            Instantiate(coinParticle,
+                this.transform.position,
+                Quaternion.Euler(-90,0,0)
+            );
             Destroy(this.gameObject);
         }
     }
